d6803d8003c574eecb8bcad31301e5bbc87657a1
ckdals7419 <37154419+ckdals7419@users.noreply.github.com>
2018-03-28 00:13:03 +0900

    Add files via upload

--- web.js
+++ web.js
@@ -6,15 +6,39 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 var seq = 0
 app.get('/log', function(req, res) {
-	fs.appendFile('log.txt', JSON.stringify(req.query)+"\n", function (err) {
+	fs.appendFile('log.csv', JSON.stringify(req.query)+"\n", function (err) {
 		if (err) throw err
 		console.log("%j", req.query)
 		res.end("Got "+ String(seq++) +" "+ JSON.stringify(req.query))
 	});
 })
 app.get('/get', function(req,res) {
-	fs.readFile('log.txt', 'utf8', function(err, data) {
+	fs.readFile('log.csv', 'utf8', function(err, data) {
 		res.send(data)
+	});
+})
+app.get('/download',function(req,res) {
+	fs.readFile('log.csv', 'utf8', function(err, data) {
+		var array = data.split("\n");
+		var length = array.length;
+		var count = parseInt(JSON.parse(JSON.stringify(req.query)).count);
+		var ans ="";
+		console.log("test : "+length);
+		console.log("data : "+data);
+
+		for(var i=0; i<length-1; i++) {
+			var value = JSON.parse(array[i]);
+		        ans += value.seq+","+ value.device+"," + value.unit+","+ value.type+","+ value.value+"," + "\n";
+			
+		}
+	
+		fs.writeFile('data.txt',ans,"utf8",function(err){
+		console.log("write success.");
+		});
+		fs.readFile('data.txt', 'utf8', function(err, data) {
+			var filename = __dirname + '/data.txt'	
+			res.download(filename);
+		});
 	});
 })
 
